36c8201c96920208f1cc90a7d606eb4547a9b3d5
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-09-25 21:59:19 +0300

    feature: добавить логирование и requirements.txt

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 import vk_api
 import time
 import requests
+import logging
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
-from vk_api.exceptions import Captcha
 from config import TOKEN, CHAT_IDS, GROUP_ID, access_token, GROUP_IDS, access_token_andr, GROUP_IDS_andr
+
+# Настройка логирования
+logging.basicConfig(
+    filename='bot_log.log',  # Файл для логов
+    level=logging.INFO,  # Уровень логирования
+    format='%(asctime)s - %(message)s',  # Формат: время и сообщение
+    datefmt='%Y-%m-%d %H:%M:%S'  # Формат времени
+)
 
 # Авторизация для пользователя
 vk_session = vk_api.VkApi(token=access_token_andr)
@@ -25,13 +33,12 @@ def send_message(message, delay=5):
             message=message,
             random_id=get_random_id()
         )
-        print(f"Сообщение отправлено в беседу {group_id}")
+        logging.info(f"Сообщение отправлено в беседу {group_id}: {message}")
         time.sleep(delay)  # Задержка между отправкой сообщений
-
 
 # Основная функция работы бота
 def main():
-    print("Бот запущен и ожидает сообщений...")
+    logging.info("Бот запущен и ожидает сообщений...")
     try:
         for event in longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
@@ -39,10 +46,11 @@ def main():
                 # Проверяем, что сообщение отправлено в ЛС (отправитель и получатель совпадают)
                 if message_info['peer_id'] == message_info['from_id']:
                     new_message = message_info['text']
-                    print(f"Новое сообщение в боте: {new_message}")
+                    logging.info(f"Новое сообщение в боте: {new_message}")
 
                     send_message(new_message)
     except requests.exceptions.ReadTimeout:
+        logging.error("Ошибка подключения (ReadTimeout), перезапуск через 5 секунд...")
         time.sleep(5)  # Задержка при ошибке подключения
         main()
 
